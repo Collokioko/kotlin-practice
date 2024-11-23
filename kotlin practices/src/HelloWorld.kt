4096fdc616efd6988ee58d7d25157c663eb2c18f
Collins Muteti <mutetic6@gmail.com>
2024-11-23 12:01:19 +0300

    Initial commit

---
+++ kotlin practices/src/HelloWorld.kt
@@ -0,0 +1,15 @@
+fun main(){
+//    println("Hello World!")
+//    var greeting= "Hello World"
+//        greeting= "Kotlin is fun"
+//        greeting= "IST"
+//    val x=20;
+//     val y=30;
+//       val sum=x+y;
+//   println(sum)
+
+val pie=3.142;
+val radius=28;
+ val area=pie * radius *radius;
+println(area)
+}
